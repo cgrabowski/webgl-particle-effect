ea1a958186c225ec14b62a3a25b9de41a8ad306b
Christopher Grabowski <yourchristopher6334@gmail.com>
2013-07-04 16:22:46 -0400

    Fixed window resize handling.

--- index.php
+++ index.php
@@ -8,12 +8,8 @@
     <script src="js/gl-matrix.js"></script>
     <script src="js/webgl-utils.js"></script>
     <script src='js/particle-effect.js'></script>
-    <script src="js/wrapper.js"></script>
     <script src='js/engine.js'></script>    
   </head>
-  <body>
-    <!--<canvas id="webgl-canvas" style="border: none;" width="500" height="500"></canvas>   --> 
-    <div id="container"></div>
-    <script src="js/test.js"></script>
+  <body onload="engine()">      
   </body>
 </html>
--- js/engine.js
+++ js/engine.js
@@ -1,21 +1,31 @@
+
+
 function engine (canvas, emittersOpts, engineCallback) {
+
+  if (!canvas) {
+    canvas = document.createElement('canvas')
+    canvas.width = window.innerWidth
+    canvas.height = canvas.width / 16 * 9
+    document.body.appendChild(canvas)
+  }
+
   try {
     gl = canvas.getContext('experimental-webgl')
-    gl.viewportWidth = canvas.width
-    gl.viewportHeight = canvas.height
   } catch (e) {
     console.error(e.message)
   }
   if (!gl)
     throw new Error('Could not initialize WebGL :-O')
 
+  gl.viewportWidth = canvas.width
+  gl.viewportHeight = canvas.height
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight)
 
   var scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(60, gl.viewportWidth / gl.viewportHeight, 1, 1000.0)
-  var renderer = new THREE.WebGLRenderer({canvas: canvas})
-    , effect = new ParticleEffect(gl, {camera: camera}, null, effectCallback)
-  controls = new THREE.TrackballControls(camera)
+    , camera = new THREE.PerspectiveCamera(60, gl.viewportWidth / gl.viewportHeight, 1, 1000.0)
+    , renderer = new THREE.WebGLRenderer({canvas: canvas})
+    , effect = new ParticleEffect(gl, {camera: camera}, emittersOpts, effectCallback)
+    , controls = new THREE.TrackballControls(camera)
 
   controls.rotateSpeed = 1.0
   controls.zoomSpeed = 10
@@ -25,6 +35,17 @@ function engine (canvas, emittersOpts, engineCallback) {
   controls.staticMoving = true
   controls.dynamicDampingFactor = 0
   controls.target.set(0, 0, -10)
+
+  function resize (event) {
+    canvas.width = window.innerWidth
+    canvas.height = canvas.width / 16 * 9
+    gl.viewPortWidth = canvas.width
+    gl.viewportHeight = canvas.height
+    gl.viewport(0, 0, gl.viewPortWidth, gl.viewportHeight)
+    controls.handleResize()
+  }
+  resize()
+  window.addEventListener('resize', resize, false)
 
   // reset camera
   window.addEventListener('keydown', function (event) {
@@ -41,7 +62,10 @@ function engine (canvas, emittersOpts, engineCallback) {
   function effectCallback () {
     scene.add(effect)
     renderer.addPostPlugin(effect)
-    engineCallback(effect, render)
+    if (engineCallback)
+      engineCallback(effect, render)
+    else
+      render()
   }
 
   function render () {
@@ -49,6 +73,5 @@ function engine (canvas, emittersOpts, engineCallback) {
     renderer.render(scene, camera)
     controls.update()
   }
-
 }
 
